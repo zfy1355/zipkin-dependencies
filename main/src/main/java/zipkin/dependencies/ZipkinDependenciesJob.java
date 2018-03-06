525405520ad5c704c2e76a4a4e3a47a2422fac7a
zhangfengyang <zhangfengyang@jd.com>
2018-03-06 14:40:25 +0800

    【定时执行任务，打印日志输出】

--- main/src/main/java/zipkin/dependencies/ZipkinDependenciesJob.java
+++ main/src/main/java/zipkin/dependencies/ZipkinDependenciesJob.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2016-2017 The OpenZipkin Authors
+ * Copyright 2016-2018 The OpenZipkin Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -13,17 +13,27 @@
  */
 package zipkin.dependencies;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import zipkin.dependencies.cassandra.CassandraDependenciesJob;
+import zipkin.dependencies.elasticsearch.ElasticsearchDependenciesJob;
+import zipkin.dependencies.mysql.MySQLDependenciesJob;
+
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLDecoder;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.TimeZone;
-import zipkin.dependencies.cassandra.CassandraDependenciesJob;
-import zipkin.dependencies.elasticsearch.ElasticsearchDependenciesJob;
-import zipkin.dependencies.mysql.MySQLDependenciesJob;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public final class ZipkinDependenciesJob {
+  private static final Logger LOGGER = LoggerFactory.getLogger(ZipkinDependenciesJob.class);
+  private static final int ONE_DAY = 24*60*60;
+  private static final int ONE_MINUTE = 60;
   /** Runs with defaults, starting today */
   public static void main(String[] args) throws UnsupportedEncodingException {
     String jarPath = pathToUberJar();
@@ -38,41 +48,63 @@ public final class ZipkinDependenciesJob {
     Runnable logInitializer = LogInitializer.create(zipkinLogLevel);
     logInitializer.run(); // Ensures local log commands emit
 
-    switch (storageType) {
-      case "cassandra":
-        CassandraDependenciesJob.builder()
-            .logInitializer(logInitializer)
-            .jars(jarPath)
-            .day(day)
-            .build()
-            .run();
-        break;
-      case "cassandra3":
-        zipkin.dependencies.cassandra3.CassandraDependenciesJob.builder()
-            .logInitializer(logInitializer)
-            .jars(jarPath)
-            .day(day)
-            .build()
-            .run();
-        break;
-      case "mysql":
-        MySQLDependenciesJob.builder()
-            .logInitializer(logInitializer)
-            .jars(jarPath)
-            .day(day)
-            .build()
-            .run();
-        break;
-      case "elasticsearch":
-        ElasticsearchDependenciesJob.builder()
-            .logInitializer(logInitializer)
-            .jars(jarPath)
-            .day(day)
-            .build()
-            .run();
-        break;
-      default:
-        throw new UnsupportedOperationException("Unsupported STORAGE_TYPE: " + storageType);
+    Runnable runnable =  new Runnable() {
+      public void run() {
+        try {
+          switch (storageType) {
+            case "cassandra":
+              CassandraDependenciesJob.builder()
+                      .logInitializer(logInitializer)
+                      .jars(jarPath)
+                      .day(day)
+                      .build()
+                      .run();
+              break;
+            case "cassandra3":
+              zipkin.dependencies.cassandra3.CassandraDependenciesJob.builder()
+                      .logInitializer(logInitializer)
+                      .jars(jarPath)
+                      .day(day)
+                      .build()
+                      .run();
+              break;
+            case "mysql":
+              MySQLDependenciesJob.builder()
+                      .logInitializer(logInitializer)
+                      .jars(jarPath)
+                      .day(day)
+                      .build()
+                      .run();
+              break;
+            case "elasticsearch":
+              ElasticsearchDependenciesJob.builder()
+                      .logInitializer(logInitializer)
+                      .jars(jarPath)
+                      .day(day)
+                      .build()
+                      .run();
+              LOGGER.info("elasticSearch job run time:" + Calendar.getInstance().getTime());
+              System.out.println("elasticSearch job run time:" + Calendar.getInstance().getTime());
+              break;
+            default:
+              throw new UnsupportedOperationException("Unsupported STORAGE_TYPE: " + storageType);
+          }
+        }catch (Exception e){
+          if(e instanceof UnsupportedOperationException)
+            throw e;
+          else
+            LOGGER.error(e.getMessage());
+        }
+      }
+    };
+    ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+    String rate = System.getenv("JOB_RATE");
+    if(rate == null){
+      service.scheduleAtFixedRate(runnable
+              , 0, ONE_MINUTE * 5, TimeUnit.SECONDS);
+    }else{
+      service.scheduleAtFixedRate(runnable
+              , 0, Integer.parseInt(rate), TimeUnit.SECONDS);
     }
   }
 
@@ -88,7 +120,7 @@ public final class ZipkinDependenciesJob {
       return df.parse(formattedDate).getTime();
     } catch (ParseException e) {
       throw new IllegalArgumentException(
-          "First argument must be a yyyy-MM-dd formatted date. Ex. 2016-07-16");
+              "First argument must be a yyyy-MM-dd formatted date. Ex. 2016-07-16");
     }
   }
 }
